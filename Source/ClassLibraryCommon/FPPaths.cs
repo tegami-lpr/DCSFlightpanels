324c9f3e4d28ffe10a0bedeeb57725f17bef42ce
Tegami <marie.osmos@outlook.com>
2021-09-22 01:42:01 +0300

    Classes for working with paths and logs added.

---
+++ Source/ClassLibraryCommon/FPPaths.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.IO;
+
+namespace ClassLibraryCommon
+{
+    /// <summary>Class <c>Paths</c> used to determine paths to application and user data.</summary>
+    public static class FPPaths
+    {
+        /// <summary>
+        /// Enum of data path types
+        /// </summary>
+        public enum EDataPath
+        {
+            /// <summary>
+            /// Path to mutable user's data
+            /// </summary>
+            dpUser = 0,
+            /// <summary>
+            /// Application install dir
+            /// </summary>
+            dpApp = 1,
+            /// <summary>
+            /// For automatic path determinics between user or app dirs.
+            /// </summary>
+            dpAuto = 2
+        };
+
+
+        /// <summary>
+        /// Function prepend <paramref name="relativePath"/> relativePath by path where data is stored.
+        /// </summary>
+        /// <param name="relativePath">Path, relative to data dir</param>
+        /// <param name="pathType">Type of data path</param>
+        /// <returns>A string with path w/o ending delimeter.</returns>
+
+        public static string PrependPath(string relativePath, EDataPath pathType = EDataPath.dpUser)
+        {
+            string dataPath = string.Empty;
+            do
+            {
+                if (_staticPath.Length != 0)
+                {
+                    dataPath = _staticPath;
+                    break;
+                }
+
+                if (_isAppDirWritable || pathType == EDataPath.dpApp)
+                {
+                    dataPath = GetApplicationPath();
+                    break;
+                }
+
+                if (pathType == EDataPath.dpAuto)
+                {
+                    string userDirPath = PrependPath(relativePath, EDataPath.dpUser);
+                    if (IsDir(userDirPath))
+                    {
+                        return userDirPath;
+                    }
+
+                    string appDirPath = PrependPath(relativePath, EDataPath.dpApp);
+                    if (IsDir(appDirPath))
+                    {
+                        return appDirPath;
+                    }
+
+                    //if all fail, then return path relative to user dir
+                    return userDirPath;
+                }
+            } while (false);
+
+            if (relativePath.Length == 0)
+            {
+                return dataPath;
+            }
+
+            dataPath = Path.Combine(dataPath, relativePath);
+            return dataPath;
+        }
+
+        ///<summary>Determine and return path to root of application's constant data.</summary>
+        ///<returns>A string with path w/o ending delimeter.</returns>
+        public static string GetApplicationPath()
+        {
+            string appDir = RemoveEndingSeparator(AppDomain.CurrentDomain.BaseDirectory);
+            return appDir;
+        }
+
+        ///<summary>Determine and return path to root of mutable user data, such as logs, configs, profiles, etc...</summary>
+        ///<returns>A string with path w/o ending delimeter.</returns>
+        public static string GetUserDataPath()
+        {
+            string appDataDir = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            return appDataDir.Length == 0 ? string.Empty : appDataDir + "\\DCSFlightpanels";
+        }
+
+
+        public static void SetPath(string path)
+        {
+            _staticPath = path;
+        }
+        public static string GetPath()
+        {
+            return _staticPath;
+        }
+
+        /// <summary>
+        /// Forcely set writable app dir flag to <paramref name="value"/>
+        /// </summary>
+        public static void SetPortable(bool value = true)
+        {
+            _isAppDirWritable = value;
+            _portabilityChecked = true;
+        }
+
+        /// <summary>
+        /// Check if application dir is writable
+        /// </summary>
+        /// <returns>True if app dir is writable and false - if not.</returns>
+        public static bool CheckPortable()
+        {
+            if (_portabilityChecked)
+            {
+                return _isAppDirWritable;
+            }
+
+            _isAppDirWritable = false;
+
+            string tmpFilePath = PrependPath("tmp.tmp", EDataPath.dpApp);
+            try
+            {
+                FileStream tmpFile = File.Create(tmpFilePath, 100, FileOptions.DeleteOnClose);
+                tmpFile.Close();
+                _isAppDirWritable = true;
+            }
+            catch (Exception)
+            {
+                //No luck...
+            }
+            _portabilityChecked = true;
+            return _isAppDirWritable;
+        }
+
+        /// <summary>
+        /// Check, if necessary and return possibility to write to application dir.
+        /// </summary>
+        /// <returns>True is app can be portable or false if not.</returns>
+        public static bool IsPortable()
+        {
+            return _portabilityChecked == false ? CheckPortable() : _isAppDirWritable;
+        }
+
+
+        public static bool IsFileExists(string path)
+        {
+            return File.Exists(path);
+        }
+
+        public static bool IsDir(string path)
+        {
+            return Directory.Exists(path);
+        }
+
+        /// <summary>
+        /// Function remove ending separator from <paramref name="path"/>
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns>A string with path w/o ending separator</returns>
+        private static string RemoveEndingSeparator(string path)
+        {
+            if (path.EndsWith("\\") || path.EndsWith("/"))
+            {
+                path = path.Remove(path.Length - 1, 1);
+            }
+            return path;
+        }
+
+        /// <summary>
+        /// Path, forcely used as app's and user's data root, instead of deducting
+        /// </summary>
+        private static string _staticPath = string.Empty;
+        /// <summary>
+        /// Sign that the application directory is writable.
+        /// </summary>
+        private static bool _isAppDirWritable = false;
+        /// <summary>
+        /// Sign that possibility to write to app dir is checked
+        /// </summary>
+        private static bool _portabilityChecked = false;
+    }
+}
---
+++ Source/ClassLibraryCommon/Logger.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace ClassLibraryCommon
+{
+    public class Logger
+    {
+        /// <summary>
+        /// Levels of logging
+        /// </summary>
+        public enum ELogLevel
+        {
+            elErrors = 0,
+            elWarnings = 1,
+            elInfo = 2,
+            elDebug = 3
+        }
+
+        public static Logger GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Logger();
+            }
+            return _instance;
+        }
+
+        public static void SetLogLevel(ELogLevel level)
+        {
+            GetInstance()._logLevel = level;
+        }
+
+        /// <summary>
+        /// Print error message to logfile. Error messages always writed.
+        /// </summary>
+        /// <param name="message"></param>
+        public static void Error(string message)
+        {
+            message = TStamp() + " ERROR   " + message;
+            GetInstance().WriteLine(message);
+        }
+
+        public static void Warning(string message)
+        {
+            if (GetInstance()._logLevel < ELogLevel.elWarnings) return;
+            message = TStamp() + " WARNING " + message;
+            GetInstance().WriteLine(message);
+        }
+
+        public static void Info(string message)
+        {
+            if (GetInstance()._logLevel < ELogLevel.elInfo) return;
+            message = TStamp() + " INFO    " + message;
+            GetInstance().WriteLine(message);
+        }
+
+        public static void Debug(string message)
+        {
+            if (GetInstance()._logLevel < ELogLevel.elDebug) return;
+            message = TStamp() + " DEBUG   " + message;
+            GetInstance().WriteLine(message);
+        }
+
+        public void WriteLine(string message)
+        {
+            if (_fallBack)
+            {
+                WriteToDebug(message);
+                return;
+            }
+
+            _logFile.WriteLine(message);
+            _logFile.Flush();
+        }
+
+        ~Logger()
+        {
+            _logFile.Close();
+        }
+
+        private Logger()
+        {
+            string logFilePath = FPPaths.PrependPath("DCSFlightpanels.log");
+            try
+            {
+                _logFile = File.AppendText(logFilePath);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Logger: Can't create log file. Error:"+ex.Message);
+                return;
+            }
+
+            WriteLine(Environment.NewLine + "=== Log opened UTC " + TStamp());
+        }
+
+        private void WriteToDebug(string message)
+        {
+            System.Diagnostics.Debug.WriteLine(message);
+        }
+
+        private static string TStamp()
+        {
+            return DateTime.UtcNow.ToString("yyyy-MM-dd hh:mm:ss");
+        }
+
+        private static Logger _instance = null;
+        private StreamWriter _logFile = null;
+        private bool _fallBack = false;
+        private ELogLevel _logLevel = ELogLevel.elDebug;
+    }
+}
